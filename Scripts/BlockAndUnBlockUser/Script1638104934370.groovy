5928c7962719a190ed331db03f901ee86270c73b
MohammadAbedWajeez <95168326+MohammadAbedWajeez@users.noreply.github.com>
2021-12-21 09:46:10 +0200

    updates

--- Scripts/BlockAndUnBlockUser/Script1638104934370.groovy
+++ Scripts/BlockAndUnBlockUser/Script1638104934370.groovy
@@ -18,6 +18,8 @@ import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 import static org.assertj.core.api.Assertions.*
 
+
+//Send Auth reuest to save token
 AuthenticateUsingEmailAndPassword = WS.sendRequest(findTestObject('api/management/account/authenticate/Authenticate user using Email and Password', 
         [('baseUrl') : GlobalVariable.baseUrl, ('email') : 'SuperAdmin@wajeez.co', ('password') : 'wajeez@123']))
 
@@ -28,6 +30,9 @@ def AuthenticateUsingEmailAndPasswordresult = AuthenticateUsingEmailAndPasswords
 def Token = AuthenticateUsingEmailAndPasswordresult.data.token
 
 GlobalVariable.token = Token
+
+///////////////////////////////////////////////////////////////////////
+//Register new user using email and password
 
 randomBodyEmail = (('RandomEmail' + ((Math.random() * 1000) as int)) + '@gmail.com')
 
@@ -57,10 +62,8 @@ assertThat(RegisteredEmail).isEqualTo(randomBodyEmail)
 
 WS.verifyResponseStatusCode(RegisterTheUserUsingEmailAndPassword, 200)
 
-
-
-
-
+///////////////////////////////////////////////////////////////////////////////////
+// Block registered user
 
 def UserRegisteredID = RegisterTheUserUsingEmailAndPasswordresult.data.id
 GlobalVariable.UserRegisteredID = UserRegisteredID
@@ -74,9 +77,20 @@ println(BlockForTheRequestedUserresult)
 
 WS.verifyResponseStatusCode(BlockForTheRequestedUser, 200)
 
+//block again
 
+BlockForTheRequestedUseragain = WS.sendRequest(findTestObject('api/management/account/Block for the requested user',
+		[('baseUrl') : GlobalVariable.baseUrl, ('UserRegisteredID') : GlobalVariable.UserRegisteredID, ('token') : GlobalVariable.token]))
 
+def BlockForTheRequestedUseragainslurper = new groovy.json.JsonSlurper()
+def BlockForTheRequestedUseragainresult = BlockForTheRequestedUseragainslurper.parseText(BlockForTheRequestedUseragain.getResponseBodyContent())
+println(BlockForTheRequestedUseragainresult)
+def BlockAgainErrorMessage = BlockForTheRequestedUseragainresult.errors[0].message
+println(BlockAgainErrorMessage)
+WS.verifyResponseStatusCode(BlockForTheRequestedUseragain, 400)
 
+///////////////////////////////////////////////////////////////////////////////////
+// Get registered user and verify blocker
 
 GetUserDetailsUsingUserId = WS.sendRequest(findTestObject('api/management/account/user Id/Get user details using his id', 
         [('baseUrl') : GlobalVariable.baseUrl, ('token') : GlobalVariable.token, ('UserRegisteredID') : GlobalVariable.UserRegisteredID]))
@@ -92,6 +106,8 @@ GlobalVariable.BlockedUserId = BlockedUserId
 
 WS.verifyResponseStatusCode(GetUserDetailsUsingUserId, 200)
 
+////////////////////////////////////////////////////////////////////////////////////
+//Unblock the blocked user
 
 UnBlockForTheRequestedUser = WS.sendRequest(findTestObject('api/management/account/UnBlockForTheRequestedUser', 
         [('baseUrl') : GlobalVariable.baseUrl, ('BlockedUserId') : GlobalVariable.BlockedUserId, ('token') : GlobalVariable.token]))
@@ -104,6 +120,24 @@ println(UnBlockForTheRequestedUserResult)
 
 WS.verifyResponseStatusCode(UnBlockForTheRequestedUser, 200)
 
+//Unblock Again
+
+UnBlockForTheRequestedUserAgain = WS.sendRequest(findTestObject('api/management/account/UnBlockForTheRequestedUser',
+	[('baseUrl') : GlobalVariable.baseUrl, ('BlockedUserId') : GlobalVariable.BlockedUserId, ('token') : GlobalVariable.token]))
+
+def UnBlockForTheRequestedUserAgainSlurper = new groovy.json.JsonSlurper()
+
+def UnBlockForTheRequestedUserAgainResult = UnBlockForTheRequestedUserAgainSlurper.parseText(UnBlockForTheRequestedUserAgain.getResponseBodyContent())
+
+println(UnBlockForTheRequestedUserAgainResult)
+
+def UnBlockAgainErrorMessage = UnBlockForTheRequestedUserAgainResult.errors[0].message
+println(UnBlockAgainErrorMessage)
+
+WS.verifyResponseStatusCode(UnBlockForTheRequestedUserAgain, 400)
+
+///////////////////////////////////////////////////////////////////////////////////
+//Recall unblocked registered user
 
 GetUserDetailsUsingUserIdafter = WS.sendRequest(findTestObject('api/management/account/user Id/Get user details using his id',
 	[('baseUrl') : GlobalVariable.baseUrl, ('token') : GlobalVariable.token, ('UserRegisteredID') : GlobalVariable.UserRegisteredID]))
@@ -115,5 +149,40 @@ println(GetUserDetailsUsingUserIdafterResult)
 
 WS.verifyResponseStatusCode(GetUserDetailsUsingUserIdafter, 200)
 
+////////////////////////////////////////////////////////////////////////////////////
+//Register User withput filling password scenarios
 
+UN_RegisterTheUserUsingEmailAndPassword = WS.sendRequest(findTestObject('api/management/account/user Id/Register the user using email and password',
+	[('baseUrl') : GlobalVariable.baseUrl, ('randomBodyEmail') : GlobalVariable.randomBodyEmail, ('randomFirstName') : GlobalVariable.randomFirstName
+		, ]))
+def UN_RegisterTheUserUsingEmailAndPasswordSlurper = new groovy.json.JsonSlurper()
+
+def UN_RegisterTheUserUsingEmailAndPasswordResult =UN_RegisterTheUserUsingEmailAndPasswordSlurper.parseText(UN_RegisterTheUserUsingEmailAndPassword.getResponseBodyContent())
+
+println(UN_RegisterTheUserUsingEmailAndPasswordResult)
+
+//Assert Email exist
+def ErrorMessage = UN_RegisterTheUserUsingEmailAndPasswordResult.errors[0].message
+println(ErrorMessage)
+assertThat("Email Already Exist").isEqualTo(ErrorMessage)
+WS.verifyResponseStatusCode(UN_RegisterTheUserUsingEmailAndPassword, 400)
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+//send epty request
+
+//Empty_RegisterTheUserUsingEmailAndPassword =WS.sendRequest(findTestObject('api/management/account/user Id/Register the user using email and password', 
+//        [('baseUrl') : GlobalVariable.baseUrl, (null) : GlobalVariable.randomBodyEmail, (null) : GlobalVariable.randomFirstName
+//            , (null) : GlobalVariable.RegisterUserPassword]))
+//def Empty_RegisterTheUserUsingEmailAndPasswordSlurper = new groovy.json.JsonSlurper()
+//
+//def Empty_RegisterTheUserUsingEmailAndPasswordResult = Empty_RegisterTheUserUsingEmailAndPasswordSlurper.parseText(Empty_RegisterTheUserUsingEmailAndPassword.getResponseBodyContent())
+//
+//println(Empty_RegisterTheUserUsingEmailAndPasswordResult)
+//
+////Assert Email exist
+//def PasswordErrorMessage = UN_RegisterTheUserUsingEmailAndPasswordResult.errors[1].message
+//println(PasswordErrorMessage)
+//assertThat("Unexpected character encountered while parsing value: ,. Path 'password', line 10, position 15.").isEqualTo(PasswordErrorMessage)
+//WS.verifyResponseStatusCode(Empty_RegisterTheUserUsingEmailAndPassword, 400)
 
